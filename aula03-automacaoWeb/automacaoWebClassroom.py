f9a6b98125b3c71f632c5b27ab091717f7bda289
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-16 10:27:23 -0300

    atualizacao lendo a tabela de commodities

--- aula03-automacaoWeb/automacaoWebClassroom.py
+++ aula03-automacaoWeb/automacaoWebClassroom.py
@@ -4,18 +4,28 @@
 # firefox -> geckodriver
 # chrome -> chromedriver
 
-from selenium import webdriver
+from selenium import webdriver # importa o modulo de utilizacao do browser do selenium
 import time
 
-navegador=webdriver.Chrome()
-
-time.sleep(5)
-# navegador=webdriver.Firefox()
 
 # Passo a passo
 
-# Passo 1: Abrir o navegador
+## Passo 1: Abrir o navegador
+
+navegador=webdriver.Chrome()
+navegador.get('https://www.google.com/')
+
+time.sleep(1)
+# navegador=webdriver.Firefox()
+
+
 # Passo 2: Importar a base de dados
+
+import pandas as pd
+
+tabela=pd.read_excel('C:\\Users\\jharbes\\Documents\\GitHub\\hashtagPython\\aula03-automacaoWeb\\commodities.xlsx')
+print(tabela)
+
 # Passo 3: Para cada produto da base de dados
 # Passo 4: Pesquisar o preco do produto
 # Passo 5: atualizar o preço na base de dados
