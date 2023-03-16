de6fa64c97c936a49f25f56014a059efb62e12f3
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-16 09:35:28 -0300

    passo a passo projeto 3 criado

---
+++ aula03-automacaoWeb/automacaoWebClassroom.py
@@ -0,0 +1,8 @@
+# Passo a passo
+
+# Passo 1: Abrir o navegador
+# Passo 2: Importar a base de dados
+# Passo 3: Para cada produto da base de dados
+# Passo 4: Pesquisar o preco do produto
+# Passo 5: atualizar o preço na base de dados
+# Passo 6: Decidir quais produtos comprar
