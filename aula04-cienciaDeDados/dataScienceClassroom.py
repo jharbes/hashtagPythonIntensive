8743bf0fc4cc242ffd16d73092d7a71606f2e665
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-03-17 13:02:09 -0300

    Update dataScienceClassroom.py

--- aula04-cienciaDeDados/dataScienceClassroom.py
+++ aula04-cienciaDeDados/dataScienceClassroom.py
@@ -8,16 +8,14 @@ Projeto Ciência de Dados - Previsão de Preços
 
 """
 
-"""
+# Passo a Passo de um Projeto de Ciência de Dados
 
-Passo a Passo de um Projeto de Ciência de Dados
+# Passo 1: Entendimento do Desafio
+    # Prever o preço de um barco baseado nas caracteristicas dele: ano, material, usado/novo, etc.
 
-- Passo 1: Entendimento do Desafio
-- Passo 2: Entendimento da Área/Empresa
-- Passo 3: Extração/Obtenção de Dados
-- Passo 4: Ajuste de Dados (Tratamento/Limpeza)
-- Passo 5: Análise Exploratória
-- Passo 6: Modelagem + Algoritmos (Aqui que entra a Inteligência Artificial, se necessário)
-- Passo 7: Interpretação de Resultados
-
-"""
+# Passo 2: Entendimento da Área/Empresa
+# Passo 3: Extração/Obtenção de Dados
+# Passo 4: Ajuste de Dados (Tratamento/Limpeza)
+# Passo 5: Análise Exploratória
+# Passo 6: Modelagem + Algoritmos (Aqui que entra a Inteligência Artificial, se necessário)
+# Passo 7: Interpretação de Resultados
