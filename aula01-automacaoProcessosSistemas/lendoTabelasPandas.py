a004d1e0980d5a6bf494d50ee79d105b3342086f
Jorge Harbes <jharbes@hotmail.com>
2023-03-18 12:13:32 -0300

    Create lendoTabelasPandas.py

---
+++ aula01-automacaoProcessosSistemas/lendoTabelasPandas.py
@@ -0,0 +1,7 @@
+# Passo 4: Calcular os indicadores
+
+import pandas
+
+# raw string
+tabela=pandas.read_csv(r'C:\Users\Jorge\Desktop\hashtagPython\aula01-automacaoProcessosSistemas\Compras.csv')
+print(tabela)
