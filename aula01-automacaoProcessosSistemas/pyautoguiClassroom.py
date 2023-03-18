79fbaaceb2134872390658d90c28c1c8021c8985
Jorge Harbes <jharbes@hotmail.com>
2023-03-18 13:14:55 -0300

    calculo dos indicadores

--- aula01-automacaoProcessosSistemas/pyautoguiClassroom.py
+++ aula01-automacaoProcessosSistemas/pyautoguiClassroom.py
@@ -88,9 +88,23 @@ pyautogui.press('enter') # faz o download
 import pandas
 
 # raw string
-tabela=pandas.read_csv(r'C:\Users\Jorge\Desktop\hashtagPython\aula01-automacaoProcessosSistemas\Compras.csv')
+# aqui indicamos no comando que o separador é o ; (ponto e virgula), pois o arquivo está usando caractere diferente do usual que é a , (virgula)
+tabela=pandas.read_csv(r'C:\Users\Jorge\Desktop\hashtagPython\aula01-automacaoProcessosSistemas\Compras.csv', sep=';')
 print(tabela)
 
+# somatorio da coluna Valor Final da tabela
+totalGasto=tabela['ValorFinal'].sum()
+
+# somatorio da coluna Quantidade da tabela
+quantidade=tabela['Quantidade'].sum()
+
+# preco medio 
+precoMedio=totalGasto/quantidade
+
+print(totalGasto)
+print(quantidade)
+print(precoMedio)
 
 
 
+print('Fim da execução')
